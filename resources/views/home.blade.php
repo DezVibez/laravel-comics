70a6ce1c0bdcae21e16a7361c282b9c05efea84d
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-15 14:42:35 +0200

    create cartelle css e js in public con npm run dev

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dc-Comics</title>
 </head>
 <body>
 
-<h1>
-    Home
-</h1>
+    <header>
+
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="">
+            
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li> 
+            </ul>
+        </div>
+        
+        
+    </header>
+    
     
 </body>
 </html>
